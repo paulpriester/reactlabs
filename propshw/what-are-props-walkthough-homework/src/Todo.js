07feef98cff6d4b4d300733fa7019b3ea821f750
Paul Priester <paulpriester507@gmail.com>
2017-11-30 18:40:57 -0500

    Update Todo.js

--- propshw/what-are-props-walkthough-homework/src/Todo.js
+++ propshw/what-are-props-walkthough-homework/src/Todo.js
@@ -4,9 +4,16 @@ import React from 'react';
 
 class Todo extends React.Component{
 
+render(){
+  debugger
   return(
-
+    <div>
+      <button onClick={(event) => this.props.alert(event)}>
+        Alert Me
+      </button>
+    </div>
   )
+}
 
 }
 
